656600f18eb6edfba91258ccef0f00b4328fad7e
GKT1 <lon06012003@gmail.com>
2022-11-11 15:01:11 +0700

    menu

---
+++ src/main/java/src/uet/oop/bomberman/ControlMenu.java
@@ -0,0 +1,19 @@
+package src.uet.oop.bomberman;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+
+
+public class ControlMenu {
+    @FXML
+    private Button button;
+
+
+    public void setButton(ActionEvent actionEvent) {
+
+        BombermanGame game = new BombermanGame();
+        game.changeSceneMenu();
+    }
+}
---
+++ src/main/java/src/uet/oop/bomberman/Info.java
@@ -0,0 +1,60 @@
+package src.uet.oop.bomberman;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import src.uet.oop.bomberman.graphics.Sprite;
+
+import java.net.URISyntaxException;
+import java.util.LinkedList;
+
+
+
+public class Info{
+    private final LinkedList<Image> info;
+    private String BOMBCONTROLLED;
+    private String SCORE;
+    private String Time;
+
+    private String LIFE;
+
+    private String FPS;
+
+    public Info(int BOMBCONTROLLED,int SCORE,int Time,int LIFE,int FPS) {
+        this.BOMBCONTROLLED = Integer.toString(BOMBCONTROLLED);
+        this.SCORE = Integer.toString(SCORE);
+        this.Time = Integer.toString(Time);
+        this.LIFE = Integer.toString(LIFE);
+        this.FPS = Integer.toString(FPS);
+        info = new LinkedList<>();
+
+        info.add(Sprite.player_down.getFxImage());
+        info.add(Sprite.bomb.getFxImage());
+    }
+
+
+    public void draw(GraphicsContext render) {
+        double x = render.getCanvas().getLayoutX() / BombermanGame.ZOOM;
+        double row = 20;
+        render.setFill(Color.BLACK);
+        render.drawImage(info.get(0), -x + 30, row - 20,30,30);
+        render.drawImage(info.get(1), -x + 95, row - 20,30,30);
+
+        render.fillText(" x", -x + 52, row);
+        render.fillText(" x", -x + 125, row);
+
+        String text = LIFE + "";
+        render.fillText(text, -x + 65, row);
+        text = BOMBCONTROLLED + "";
+        render.fillText(text, -x + 138, row);
+        Text calc = new Text(SCORE + "");
+
+        render.fillText(calc.getText(),
+                -x + 245.5 - calc.getBoundsInLocal().getWidth() / 2, row);
+        text = "TIME: " + Time + "";
+        render.fillText(text, -x + 350, row);
+        text = "FPS: " + FPS;
+        render.fillText(text, -x + 430, row);
+    }
+}
