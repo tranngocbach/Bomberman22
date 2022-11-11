e8fc3b38e8ec684693fd4130d91b058ecae84d69
bachtran <tranngocbach2003@gmail.com>
2022-11-11 15:56:17 +0700

    2 nut

--- src/main/java/src/uet/oop/bomberman/ControlMenu.java
+++ src/main/java/src/uet/oop/bomberman/ControlMenu.java
@@ -4,16 +4,30 @@ import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
+import javafx.scene.control.ToggleButton;
 
 
 public class ControlMenu {
     @FXML
     private Button button;
 
+    @FXML
+    private Button guide;
+
+    @FXML
+    private Button exit;
 
     public void setButton(ActionEvent actionEvent) {
 
         BombermanGame game = new BombermanGame();
         game.changeSceneMenu();
+    }
+
+    public void openGuide(ActionEvent actionEvent) {
+        System.out.println("OK");
+    }
+
+    public void Exit(ActionEvent actionEvent) {
+        System.out.println("OK");
     }
 }
